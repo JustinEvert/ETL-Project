51b7cf8a7c952f45706e67f83b1fb43c870864f1
Jpkemperor <jan_jacm123@hotmail.com>
2019-08-22 20:32:53 -0500

    updating notebook and queries

--- Consolidated.sql
+++ Consolidated.sql
@@ -12,3 +12,50 @@ CREATE TABLE consolidated (
   acciones FLOAT,
   monto FLOAT
 );
+
+CREATE TABLE fonden (
+  id INTEGER,
+  Estado VARCHAR,
+  Municipio VARCHAR,
+  Evento VARCHAR,
+  Tipo VARCHAR, 
+  Accion VARCHAR,
+  Año INTEGER,
+  Mes VARCHAR
+);
+
+SELECT * FROM fonden;
+
+CREATE VIEW acciones_por_estado AS
+SELECT estado, SUM(acciones) AS total_acciones
+FROM consolidated
+GROUP BY estado;
+
+CREATE VIEW monto_por_estado AS
+SELECT estado, SUM(monto) AS total_monto
+FROM consolidated
+GROUP BY estado;
+
+SELECT * FROM acciones_por_estado;
+SELECT * FROM monto_por_estado;
+
+CREATE VIEW estados_agrupados AS
+SELECT a.estado, a.total_acciones, m.total_monto
+FROM acciones_por_estado a
+JOIN monto_por_estado m
+ON a.estado = m.estado;
+
+SELECT * FROM estados_agrupados;
+
+CREATE VIEW desastres_por_estado AS
+SELECT estado, COUNT(evento) AS total_desastres
+FROM fonden
+GROUP BY estado;
+
+SELECT * FROM desastres_por_estado;
+
+SELECT e.estado, e.total_acciones, e.total_monto, d.total_desastres
+FROM estados_agrupados e
+LEFT JOIN desastres_por_estado d
+ON e.estado = d.estado
+ORDER BY total_desastres;
